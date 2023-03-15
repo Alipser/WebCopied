0895408b16d484763b21aa2f111ee4c3647460b9
Romario André Julio <romarioajb@outlook.com>
2023-03-15 18:05:29 -0500

    Update Aleluya.js

--- Copy/www.classcentral.com/webpack/Aleluya.js
+++ Copy/www.classcentral.com/webpack/Aleluya.js
@@ -1,15 +1,15 @@
 console.log("Aleluya DEEp")
 
-const button = document.querySelector('#page-home > div.contain-page > header > div.relative.bg-white.cc-header.border-bottom.border-gray-light > nav > div.margin-right-small.large-up-margin-right-medium > button.hidden.weight-semi.large-up-block.text-1.color-charcoal.padding-right-small')
+const otra = document.querySelector('#page-home > div.contain-page > header > div.relative.bg-white.cc-header.border-bottom.border-gray-light > nav > div.margin-right-small.large-up-margin-right-medium > button.hidden.weight-semi.large-up-block.text-1.color-charcoal.padding-right-small')
 
 let html = document.querySelector('html')
 
 let nav = document.querySelector('#page-home > div.contain-page > header > div.relative.bg-white.cc-header.border-bottom.border-gray-light > nav > div.margin-right-small.large-up-margin-right-medium > nav')
 let div = document.querySelector('#page-home > div.contain-page > header > div.width-100.height-100.fixed.top.left')
 let appear=document.querySelector('#page-home > div.contain-page > header > div.relative.bg-white.cc-header.border-bottom.border-gray-light > nav > div.margin-right-small.large-up-margin-right-medium > nav > div.main-nav-dropdown__index > section:nth-child(2)')
-button.addEventListener("click", reveal)
+otra.addEventListener("click", reveal)
 
-function reveal(params) {
+function reveal() {
     html.classList.toggle('nav-open')
     nav.classList.toggle('is-open')
     div.classList.toggle("animate-fade-hidden")
